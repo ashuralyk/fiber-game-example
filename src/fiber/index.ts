00deae9fac965b7e462cb8468a5d445a5cc89a4b
ashuralyk <ashuralyk@live.com>
2026-03-31 08:50:53 +0800

    feat: initial space-invador from fiber-docs

---
+++ src/fiber/index.ts
@@ -0,0 +1,70 @@
+import { FiberNode } from "./node";
+
+export const amountPerPoint = 1 * 10 ** 8; // 1 CKB per point
+
+const node1 = {
+    peerId: "QmUTsrLgHnoFenNbSFQyYAmAFxi3ZPkxAVnqJ8e9hhzeSm",
+    address:
+        "/ip4/127.0.0.1/tcp/8228/p2p/QmUTsrLgHnoFenNbSFQyYAmAFxi3ZPkxAVnqJ8e9hhzeSm",
+    url: "/node1-api",
+};
+
+const node2 = {
+    peerId: "QmTUyCohdeaWyPVSeQg5uWiTG8ztZ2rwBXP7PUZPk5Qc2w",
+    address:
+        "/ip4/127.0.0.1/tcp/8238/p2p/QmTUyCohdeaWyPVSeQg5uWiTG8ztZ2rwBXP7PUZPk5Qc2w",
+    url: "/node2-api",
+};
+
+export async function prepareNodes() {
+    const bossNode = new FiberNode(node1.url, node1.peerId, node1.address);
+    const playerNode = new FiberNode(node2.url, node2.peerId, node2.address);
+    console.log("bossNode", bossNode);
+    console.log("playerNode", playerNode);
+
+    await bossNode.sdk.connectPeer({
+        address: playerNode.address,
+    });
+
+    const channels = await bossNode.sdk.listChannels({
+        peerId: playerNode.peerId,
+    });
+    const activeChannel = channels.filter(
+        (channel) => channel.state.stateName === "CHANNEL_READY",
+    );
+    console.log("activeChannel", activeChannel);
+    return { bossNode, playerNode };
+}
+
+export async function payPlayerPoints(
+    bossNode: FiberNode,
+    playerNode: FiberNode,
+    points: number,
+) {
+    const amount = `0x${(amountPerPoint * points).toString(16)}`;
+
+    const invoice = await playerNode.createCKBInvoice(
+        amount,
+        "player hit the boss!",
+    );
+    const result = await bossNode.sendPayment(invoice.invoiceAddress);
+    console.log(`boss pay player ${points} CKB`);
+    console.log("invoice", invoice);
+    console.log("payment result", result);
+}
+
+export async function payBossPoints(
+    bossNode: FiberNode,
+    playerNode: FiberNode,
+    points: number,
+) {
+    const amount = `0x${(amountPerPoint * points).toString(16)}`;
+    const invoice = await bossNode.createCKBInvoice(
+        amount,
+        "boss hit the player!",
+    );
+    const result = await playerNode.sendPayment(invoice.invoiceAddress);
+    console.log(`player pay boss ${points} CKB`);
+    console.log("invoice", invoice);
+    console.log("payment result", result);
+}
---
+++ src/fiber/node.ts
@@ -0,0 +1,38 @@
+import { FiberSDK } from "@ckb-ccc/fiber";
+
+export class FiberNode {
+    public readonly sdk: FiberSDK;
+
+    constructor(
+        public readonly url: string,
+        public readonly peerId: string,
+        public readonly address: string,
+    ) {
+        this.sdk = new FiberSDK({ endpoint: url });
+    }
+
+    private generateRandomPaymentPreimage() {
+        const bytes = crypto.getRandomValues(new Uint8Array(32));
+        return (
+            "0x" +
+            Array.from(bytes)
+                .map((b) => b.toString(16).padStart(2, "0"))
+                .join("")
+        );
+    }
+
+    async createCKBInvoice(amount: string, description: string) {
+        const paymentPreimage = this.generateRandomPaymentPreimage();
+        return await this.sdk.newInvoice({
+            amount,
+            currency: "Fibt",
+            description,
+            expiry: "0xe10",
+            paymentPreimage,
+        });
+    }
+
+    async sendPayment(invoice: string) {
+        return await this.sdk.sendPayment({ invoice });
+    }
+}
